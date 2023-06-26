9f9bce785a40135d3a857115b610bbc2f3c0f8b3
venuv <venuv62@gmail.com>
2023-06-25 20:12:40 -0400

    initial commit for Griffin

---
+++ dashboard.py
@@ -0,0 +1,162 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import textwrap
+
+from langchain.llms import OpenAI
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationBufferMemory
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain.chat_models import ChatOpenAI
+
+import re
+from textblob import TextBlob
+
+def convert_text(text):
+    # Extract the subset after 'Story:'
+    match = re.search('Story:(.*)', text)
+    if match:
+        story = match.group(1)
+    else:
+        story = text
+
+    # Replace first person to second person
+    blob = TextBlob(story)
+
+    pronoun_dict = {'i': 'you', 'me': 'you', 'my': 'your', 'mine': 'yours', 'am': 'are', 'i\'m': 'you\'re', 
+                    'i\'ve': 'you\'ve', 'i\'d': 'you\'d', 'i\'ll': 'you\'ll', 
+                    'we': 'you', 'us': 'you', 'our': 'your', 'ours': 'yours','hi':''}
+
+    new_story = ' '.join([pronoun_dict.get(word, word) for word in blob.words.lower()])
+
+    return new_story
+
+
+def conduct_interview(persona, question_list):
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.9)
+
+    preamble = "You are an AI and are tasked with role playing and taking on the persona given to you after ###Persona. Be  helpful and informative in studying consumer needs to benefit society,  Even though the persona is specific in first person, you as an AI are to assume that persona as an AI model, and simulate that persona's decision making to the best of your abilities. \n Give minute details of your product experiences  in the responses to the questions that follow."
+    persona_begin = "###Persona\ "
+    preamble = preamble + persona_begin+convert_text(persona)+'\n'
+    print(f"PREAMBLE:\n{preamble}")
+    
+    conversation = ConversationChain(
+        llm=llm, 
+        verbose=True, 
+        memory=ConversationBufferWindowMemory(k=4)
+    )
+
+    conversation_log = []
+    question_list_size = len(question_list)
+    n = 1
+    for question in question_list:
+        print("QUESTION : ", question)
+        if n==1:
+            question = preamble + " " + question
+        response = conversation.predict(input=question)
+            #questionnaire_progress.write("completed "+ str(n)
+            #                             +" out of "+str(question_list_size))
+        print(f"Progress - {str(n)} of {str(question_list_size)}")
+        n += 1
+        conversation_log.append({"Context": question,"Response": response})
+    # your code here that generates the df dataframe
+#    df = "you crushed it!"
+    return conversation_log
+
+
+# Define the jitter function
+def jitter(arr):
+    stdev = .01*(max(arr)-min(arr))
+    return arr + np.random.randn(len(arr)) * stdev
+
+# Format the hover text
+def format_hover_text(row):
+    wrapper = textwrap.TextWrapper(width=50)
+    wrapped_story = wrapper.wrap(text=row['story'])
+    return '<br>'.join(wrapped_story)
+
+
+def plot_scatter(df):
+# Create the scatter plot with jitter
+    df['severity_num'] = df['severity'].astype('category').cat.codes
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df['jittered_region'],
+                             y=df['jittered_severity'],
+                             mode='markers',
+                             text=df['formatted_story'],
+                             hovertemplate='%{text}<extra></extra>',
+                             name = "Persona by region and bug severity",
+                             marker=dict(size=10, opacity=0.5)))
+    fig.update_xaxes(tickvals=df['region_num'].unique(),
+                     ticktext=df['region'].unique())
+    fig.update_yaxes(tickvals=df['severity_num'].unique(),
+                     ticktext=df['severity'].unique())
+
+    fig.update_layout(
+        xaxis_title="Region",
+        yaxis_title="Severity",
+        autosize=False,
+        width=700,
+        height=700,
+    )
+    return fig
+
+# Load your data
+df = pd.read_excel("population.xlsx")
+df['formatted_story'] = df.apply(format_hover_text, axis=1)
+df['region_num'] = df['region'].astype('category').cat.codes
+df['severity_num'] = df['severity'].astype('category').cat.codes
+df['jittered_region'] = jitter(df['region_num'])
+df['jittered_severity'] = jitter(df['severity_num'])
+df['info'] = df['name'] + ', Age: ' + df['age'].astype(str) + ', Story: ' + df['story']
+
+
+# Setup Streamlit layout
+st.title("Zevo Synth Dashboard")
+
+st.header("Explore Population")
+fig = plot_scatter(df)
+st.plotly_chart(fig)
+
+
+st.header("Shortlist Interviewees")
+# Here you can place your widget for shortlisting interviewees.
+# For example, a multiselect widget where you can select names from a list
+#shortlist = st.multiselect("Select Interviewees", options=df['name'].unique())
+shortlist = st.multiselect("Select Interviewees", options=sorted(df['info']))
+
+st.write("You selected these interviewees: ", shortlist)
+
+st.header("Conduct Interviews")
+# Streamlit list selector
+candidate = st.selectbox('Select a candidate:', shortlist)
+
+
+uploaded_file = st.file_uploader("Choose an XLS file", type=["xlsx","xls"])
+
+# Load_interview button
+if st.button('Load Interview'):
+#    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+    if uploaded_file is not None:
+#        data = pd.read_csv(uploaded_file)
+        data = pd.read_excel(uploaded_file, usecols=[0], engine="openpyxl")
+        column_values = data.iloc[:, 0].tolist()
+        question_list = column_values
+
+        #st.session_state.data = data  # Store data in session_state for access after re-running
+        st.session_state.data = question_list  # Store data in session_state for access after re-running
+    else:
+        st.write('No file uploaded yet')
+
+# If data exists in session_state, show it
+if 'data' in st.session_state:
+    st.write(st.session_state.data)
+    
+# Conduct_interview button
+if st.button('Conduct Interview'):
+    # Here, write the code you need to perform the interview
+    st.write(f'Conducting interview for: {candidate}')
+    df = conduct_interview(candidate, st.session_state.data)
+    st.write(df)
---
+++ utilities.py
@@ -0,0 +1,101 @@
+from langchain.chains import SimpleSequentialChain
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains import LLMChain
+import random
+import string
+import re
+
+
+def extract_name(lm, intro):
+    pt = "return the person name mentioned in  {intro}. if you are absolutely sure it is not present in the {intro}, return None"
+    xtract_prompt = ChatPromptTemplate.from_template(pt)
+    chain = LLMChain(llm=lm, prompt=xtract_prompt)
+    name = chain.run(intro)
+    return name
+
+def extract_age(lm, intro):
+    pt = "return the person age mentioned in  {intro}. if you are absolutely sure it is not present in the {intro}, return None"
+    xtract_prompt = ChatPromptTemplate.from_template(pt)
+    chain = LLMChain(llm=lm, prompt=xtract_prompt)
+    age = chain.run(intro)
+    return age
+
+def extract_city(lm, intro):
+    pt = "return the city mentioned in  {intro}. if you are absolutely sure it is not present in the {intro}, return None"
+    xtract_prompt = ChatPromptTemplate.from_template(pt)
+    chain = LLMChain(llm=lm, prompt=xtract_prompt)
+    city = chain.run(intro)
+    return city
+
+def extract_region(lm, intro, city):
+    pt = "return the region mentioned in  {intro}, or implied by the {city}. regions can be - northeast, midwest, souteast, south, southwest, west and northwest. if you are absolutely sure you cannot figure it out, return None"
+    xtract_prompt = ChatPromptTemplate.from_template(pt)
+    chain = LLMChain(llm=lm, prompt=xtract_prompt)
+    region = chain.run({'intro':intro,'city':city})
+    return region
+
+def extract_home_type(lm, intro):
+    pt = "return the home type if  mentioned in  {intro}. home type is - apartment, condo, or single family homeif you are absolutely sure it is not present in the {intro}, return None"
+    xtract_prompt = ChatPromptTemplate.from_template(pt)
+    chain = LLMChain(llm=lm, prompt=xtract_prompt)
+    hometype = chain.run(intro)    
+    return hometype
+
+def generate_random_string(length):
+    # Choose from all uppercase and lowercase letters and digits
+    chars = string.ascii_letters + string.digits
+
+    # Use a list comprehension to generate a list of 'length' random characters
+    random_string = ''.join(random.choice(chars) for _ in range(length))
+
+    return random_string
+
+
+def process_stories(stories,lm,collection):
+
+    '''
+    # Remove newlines from stories collective
+    stories_without_newlines = stories.replace("\n", " ")
+    #print(f"stories without newlines is {stories_without_newlines}")
+    stories = re.split(r'(?=Hi, I am)', stories_without_newlines)
+    '''
+
+    stories = re.split(r'(?=Hi, I am)', stories)
+    stories = [story for story in stories if re.search('[a-zA-Z]', story)]
+
+    ids = []
+    metadatas = []
+    documents = []
+    for story in stories:
+        intro_sentence = story.split(".")[0]
+        city=extract_city(lm, intro_sentence)
+        person_info = {
+            'name':extract_name(lm, intro_sentence),
+            'age':extract_age(lm,intro_sentence),
+            'city':city,
+            'region':extract_region(lm, intro_sentence,city),
+            'hometype':extract_home_type(lm, intro_sentence)
+            }
+        id = generate_random_string(8)
+        documents.append(story)
+        metadatas.append(person_info)
+        ids.append(id)
+        print(f"Metadata--{person_info}")
+        
+    collection.add(documents=documents,
+                   metadatas=metadatas,
+                   ids=ids)
+    return stories
+              
+# generate population - generate stories. extract metadata. persist to vectordb - populate collection. save collection. return stories
+def generate_population(lm, demo_chain, stories_chain, seed_prompt,collection):
+    story_chain = SimpleSequentialChain(
+        chains=[demo_chain,stories_chain],
+        verbose=True
+    )
+
+    raw_stories = story_chain.run(seed_prompt)
+    stories = process_stories(raw_stories,lm,collection)
+    
+    return stories
